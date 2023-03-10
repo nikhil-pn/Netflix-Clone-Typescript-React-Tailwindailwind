3285460db4efb5fe8bf86c97f482ddbfece07e5f
nikhilpn725 <nikhilpn725@gmail.com>
2023-03-10 11:38:35 +0530

    react router dom added and Routes "/" created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-export default function App() {
-  return (
-    <div className="grid h-screen place-content-center bg-green-500">
-      <h1 className=" justify-center  text-6xl font-semibold text-white underline ">
-        Tailwind Is Working
-      </h1>
-    </div>
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+function AppRouter() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route
+          path="/"
+          element={
+            <div className="grid h-screen place-content-center bg-green-500">
+              <h1 className=" justify-center  text-6xl font-semibold text-white underline ">
+                Tailwind Is Working
+              </h1>
+            </div>
+          }
+        ></Route>
+      </>
+    )
   );
+
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default function App() {
+  return <AppRouter />;
 }
