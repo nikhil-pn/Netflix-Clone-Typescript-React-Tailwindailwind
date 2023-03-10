ad6bb38a7664edf07ae506f14b15411ed154a555
nikhilpn725 <nikhilpn725@gmail.com>
2023-03-10 14:49:04 +0530

    netflix logo and header nav struture created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+
+import netflix from "../assets/logo.png"
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="border-b-2 py-2">
+      <nav className="grid grid-cols-[200px_auto_200px] gap-4">
+        <section>
+          <img className="h-10" src={netflix} alt="" />
+        </section>
+        <section>Primary</section>
+        <section>Secondary</section>
+      </nav>
+    </header>
+  );
 }
